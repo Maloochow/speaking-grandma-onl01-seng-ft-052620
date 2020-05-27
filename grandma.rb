7cf0f59e6752e9dc268ccb7fc5f0847ba275a115
boisterous-media-9079 <luyao.mly@gmail.com>
2020-05-27 15:41:25 +0000

    Automatically backed up by Learn

--- grandma.rb
+++ grandma.rb
@@ -6,8 +6,8 @@ def speak_to_grandma(string)
   elsif string == "I LOVE YOU GRANDMA!"
     return "I LOVE YOU TOO PUMPKIN!"
   end
-  else 
-    return "HUH?! SPEAK UP, SONNY!" 
+  else
+    return "HUH?! SPEAK UP, SONNY!"
   end
 end
 
